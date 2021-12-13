9622c418ab88502f8cfb6d22dbfbe846be532a5c
peterwilliams <peterkennethwilliams@gmail.com>
2021-12-13 10:29:52 +0000

    bookings controller finished

--- controllers/bookings_controller.py
+++ controllers/bookings_controller.py
@@ -1,4 +1,52 @@
 from flask import Blueprint, Flask, redirect, render_template, request
 from models.booking import Booking
 
+import repositories.booking_repository as booking_repostiory
+import repositories.member_repository as member_repository
+import repositories.gym_class_repository as gym_class_repository
+
 bookings_blueprint = Blueprint("bookings",__name__)
+
+@bookings_blueprint.route("/bookings")
+def bookings():
+    bookings = booking_repostiory.select_all()
+    return render_template('bookings/index.html',bookings = bookings)
+
+
+@bookings_blueprint.route("/bookings/new")
+def new_booking(id):
+    members =  member_repository.select_all()
+    gym_classes = gym_class_repository.select_all()
+    return render_template("bookings/new.html",members = members,gym_classes = gym_classes)
+
+@bookings_blueprint.route("/bookings", mehtods =["POST"])
+def create_booking():
+    member_id = request.form["member_id"]
+    gym_class_id = request.form["gym_class_id"]
+    member = member_repository.select(member_id)
+    gym_class = gym_class_repository.select(gym_class_id)
+
+    new_booking = Booking(member,gym_class)
+    booking_repostiory.save(new_booking)
+    return redirect("/bookings")
+
+@bookings_blueprint.route("/bookings/<id>",methods = ["POST"])
+def update_booking(id):
+    member_id = request.form["member_id"]
+    gym_class_id = request.form["gym_class_id"]
+    member = member_repository.select(member_id)
+    gym_class = gym_class_repository.select(gym_class_id)
+    booking = Booking(member,gym_class)
+    booking_repostiory.update(booking)
+    return redirect ("/bookings")
+
+@bookings_blueprint.route("/bookings/<id>/delete",methods =["POST"])
+def delete_booking(id):
+    booking_repostiory.delete(id)
+    return redirect("/bookings")
+
+    
+    
+  
+
+
--- controllers/gym_classes_controller.py
+++ controllers/gym_classes_controller.py
@@ -1,5 +1,29 @@
 from flask import Blueprint, Flask, redirect, render_template, request
 from models.gym_class import GymClass
+import repositories.staff_repository as staff_repository
+import repositories.gym_class_repository as gym_class_repository
 
 gym_classes_blueprint =Blueprint("gym_classes",__name__)
+
+@gym_classes_blueprint.route("/gym_classes")
+def gym_classes():
+    gym_classes = gym_class_repository.select_all
+    return render_template("gym_classes/index.html", gym_classes =  gym_classes)
+
+@gym_classes_blueprint.route("/gym_classes/<id>")
+def show_gym_class(id):
+    members = gym_class_repository.select_members_in_class(id)
+    gym_class = gym_class_repository.select(id)
+    return render_template("gym_classes/show.html")
+
+@gym_classes_blueprint.route("/gym_classes/new")
+def new_gym_class():
+    staff = staff_repository.select_all()
+    return render_template("gym_classes/new.html")
+    
+
+
+
+
+    
 
